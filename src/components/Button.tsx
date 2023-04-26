4f965cd236e7595e35a4ec72622d0e1b188a1eac
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-04-26 11:46:17 +0100

    I worked on some styling and responsiveness ont the landing page

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,23 +1,21 @@
-import React from "react";
-import clsx from "clsx";
+import React from 'react';
+import clsx from 'clsx';
 
 interface ButtonProps {
-  variant?: "primary" | "outline";
+  variant?: 'primary' | 'outline';
   className?: string;
   children: React.ReactNode;
 }
 
-export const Button: React.FC<ButtonProps> = ({
-  variant = "primary",
-  className,
-  children,
-}) => {
+export const Button: React.FC<ButtonProps> = ({ variant = 'primary', className, children }) => {
   return (
     <button
       className={clsx(
-        "border-primary-100 font-open-sans rounded-full border px-6 py-3 font-semibold",
-        variant === "primary" && "bg-primary-100 text-white",
-        variant === "outline" && "text-gray-950",
+        'rounded-full border border-primary-100 px-6 py-3 font-open-sans font-semibold',
+        variant === 'primary' && 'bg-primary-100 text-white',
+        variant === 'primary' && 'hover:bg-primary-300',
+        variant === 'outline' && 'text-gray-950',
+        variant === 'outline' && ' hover:bg-primary-300 hover:text-white',
         className
       )}
     >
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,18 +17,18 @@ const Home: NextPage = () => {
           style={{ backgroundImage: "url('/assets/home-hero.png')" }}
           className="h-[calc(100vh-96px)] bg-cover bg-no-repeat text-white"
         >
-          <div className="mx-auto flex h-full max-w-3xl flex-col items-center justify-center p-16 px-5 text-center sm:px-10 md:px-0">
-            <h1 className="font-lato text-5xl font-black">
+          <div className="container mx-auto flex h-full flex-col items-center justify-center p-16 px-5 text-center sm:max-w-3xl sm:px-10 md:px-0">
+            <h1 className="font-lato text-2xl sml:text-4xl sm:font-black md:text-5xl">
               Making Your <span className="text-primary-100">Space</span> Spotless with Expert
               Cleaning Solutions.
             </h1>
-            <p className="my-6 text-2xl font-medium">
+            <p className="my-6 text-lg">
               At Trevone, we believe that a clean space is a happy space. That's why we offer a wide
               range of cleaning services to help keep your home or office looking its best.
             </p>
-            <div className="flex items-center space-x-5">
-              <Button>Book us online now</Button>
-              <LinkButton href="/" className="text-white" variant="outline">
+            <div className="sml:flex sml:space-x-5">
+              <Button className="mb-4 xm:w-full sml:mb-0">Book us online now</Button>
+              <LinkButton href="/" className="text-white xm:w-full sm:w-max" variant="outline">
                 Learn more about us
               </LinkButton>
             </div>
